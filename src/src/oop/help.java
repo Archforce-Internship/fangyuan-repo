f7df15084a59282a15f5b07542d86e038f836f98
183872864 <183872864@qq.com>
2021-03-02 20:57:38 +0800

    11

--- src/src/oop/help.java
+++ src/src/oop/help.java
@@ -4,9 +4,8 @@ public class help {
     public static void main(String[] args) {
         father f = new father();
         f.fa1();
-        son2 two = new son2("1","2",3);
+        son2 two = new son2("1", "2", 3);
         two.fa1();
         two.two();
-
     }
 }
